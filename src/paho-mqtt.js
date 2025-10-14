62deef318e828d7eb6a1c0c82e0c37e4ee4412db
Gerd Stolpmann <gerd@figlyinc.com>
2025-10-14 23:27:29 +0200

    sendPinger: do not disconnect when there is ping response (this is just not the task of the sendPinger!)

--- src/paho-mqtt.js
+++ src/paho-mqtt.js
@@ -700,7 +700,7 @@ function onMessageArrived(message) {
 	 * Repeat keepalive requests, monitor responses.
 	 * @ignore
 	 */
-		var Pinger = function(client, keepAliveInterval) {
+	    var Pinger = function(client, keepAliveInterval, noDisconnect) {
 			this._client = client;
 			this._keepAliveInterval = keepAliveInterval*1000;
 			this.isReset = false;
@@ -719,7 +719,7 @@ function onMessageArrived(message) {
 					this._client._trace("Pinger.doPing", "Timed out");
 					this._client._disconnected( ERROR.PING_TIMEOUT.code , format(ERROR.PING_TIMEOUT));
 				} else {
-					this.isReset = false;
+					this.isReset = noDisconnect;
 					this._client._trace("Pinger.doPing", "send PINGREQ");
 					this._client.socket.send(pingReq);
 					this.timeout = setTimeout(doTimeout(this), this._keepAliveInterval);
@@ -1059,8 +1059,8 @@ function onMessageArrived(message) {
 			this.socket.onerror = scope(this._on_socket_error, this);
 			this.socket.onclose = scope(this._on_socket_close, this);
 
-			this.sendPinger = new Pinger(this, this.connectOptions.keepAliveInterval);
-			this.receivePinger = new Pinger(this, this.connectOptions.keepAliveInterval);
+		    this.sendPinger = new Pinger(this, this.connectOptions.keepAliveInterval, true);
+		    this.receivePinger = new Pinger(this, this.connectOptions.keepAliveInterval, false);
 			if (this._connectTimeout) {
 				this._connectTimeout.cancel();
 				this._connectTimeout = null;
